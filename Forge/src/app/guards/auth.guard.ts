66a1d85fcb9a35a568a8a03cef319fd875ac2acf
Ric <ricardocarr0817@gmail.com>
2020-10-15 15:27:27 -0700

    Added Auth Guard

---
+++ Forge/src/app/guards/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  router: any;
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.isLoggedIn()){
+         return true;
+      }
+     this.router.navigateByURL['/login'];
+     return false;
+  }
+  public isLoggedIn():boolean {
+    let status  = false;
+    if(localStorage.getItem('LoggedIn')=="true"){
+      status = true;
+    }
+    else{
+      status = false;
+    }
+    return status;
+  }
+  
+}
