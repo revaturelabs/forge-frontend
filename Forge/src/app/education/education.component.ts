ce6c69a8bb8919a5fcc491a14a64b2413c9fef29
Gilbert Mwangi <mwangigilbert14@gmail.com>
2020-10-20 14:48:29 -0500

    education form complete

--- Forge/src/app/education/education.component.ts
+++ Forge/src/app/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-education',
@@ -8,8 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class EducationComponent implements OnInit {
 
   constructor() { }
+  
+  maxdate = new Date();
+  mindate = new Date(1973, 0, 1);
+
+  degrees:string[] = ["Associate's Degree", "Bachelor's Degree", "Master's Degree", "PhD"];
+
+  portfolioForm = new FormGroup({
+    degree: new FormControl('', Validators.required),
+    university: new FormControl('', Validators.required),
+    graduationDate: new FormControl('', Validators.required),
+    major: new FormControl('', Validators.required),
+    minor: new FormControl('')
+  });
+
+  onSubmit(){
+    console.log("I currently cant submit anything");
+  }
 
   ngOnInit(): void {
   }
-
 }
