3aaf8df4a69e024f58e2e5408da26e201c7917b2
Gilbert Mwangi <mwangigilbert14@gmail.com>
2020-10-21 15:01:34 -0500

    testing implemented for eductation component

--- Forge/src/app/education/education.component.ts
+++ Forge/src/app/education/education.component.ts
@@ -16,15 +16,22 @@ export class EducationComponent implements OnInit {
   degrees:string[] = ["Associate's Degree", "Bachelor's Degree", "Master's Degree", "PhD"];
 
   portfolioForm = new FormGroup({
-    degree: new FormControl('', Validators.required),
-    university: new FormControl('', Validators.required),
+    degree: new FormControl('Select a Degree', Validators.required),
+    university: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(40)] ),
     graduationDate: new FormControl('', Validators.required),
-    major: new FormControl('', Validators.required),
-    minor: new FormControl('')
+    major: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(40)]),
+    minor: new FormControl('', [Validators.minLength(3), Validators.maxLength(40)])
   });
+
+  public noWhitespaceValidator(control: FormControl) {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { 'whitespace': true };
+  }
 
   onSubmit(){
     console.log("I currently cant submit anything");
+    console.log(this.portfolioForm.value);
   }
 
   ngOnInit(): void {
