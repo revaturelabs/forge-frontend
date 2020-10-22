989306382e610e50a995c360908470e26247015c
DMattDraper <69763575+DMattDraper@users.noreply.github.com>
2020-10-22 19:47:30 -0400

    Began adding testing to industry equivalency

--- Forge/src/app/industry-equivalency/industry-equivalency.component.spec.ts
+++ Forge/src/app/industry-equivalency/industry-equivalency.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { IndustryEquivalencyComponent } from './industry-equivalency.component';
 
@@ -21,5 +22,37 @@ describe('IndustryEquivalencyComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call addLabel', () => {
+    spyOn(component, 'addLabel');
+
+    let button: HTMLSelectElement = fixture.debugElement.query(By.css('.add-button')).nativeElement;
+    button.click();
+
+    expect(component.addLabel).toHaveBeenCalled();
+  });
+
+  it('should subtractLabel', () => {
+    spyOn(component, 'subtractLabel');
+
+    let button: HTMLSelectElement = fixture.debugElement.query(By.css('.subtract-button')).nativeElement;
+    button.click();
+
+    expect(component.subtractLabel).toHaveBeenCalled();
+  });
+
+
+  it('should add skill', () => {
+    spyOn(component, 'subtractLabel');
+
+    component.skill = "JavaScript";
+    component.experience = 6;
+
+    let button: HTMLSelectElement = fixture.debugElement.query(By.css('.add-button')).nativeElement;
+    button.click();
+
+    expect(component.barChartData[0].data.pop).toEqual(6);
+    expect(component.barChartLabels.pop).toEqual("JavaScript");
   });
 });
