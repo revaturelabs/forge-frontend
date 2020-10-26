ceeebd4d07aef302d4bd76b076b0e640c934ea62
Ric <ricardocarr0817@gmail.com>
2020-10-25 19:34:50 -0700

    navbar functionality

--- Forge/src/app/app-routing.module.ts
+++ Forge/src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   {path:'project', component: ProjectsComponent},
   {path:'aboutMe', component: AboutMeComponent},
-  {path:'portfolio', component: PortfolioComponent}
+  {path:'portfolio', component: PortfolioComponent},
+  {path:'navbar', component: NavbarComponent, canActivate: [AuthGuard]},
+  {path:'logout', component: LoginComponent},
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Forge/src/app/service/auth-service.service.ts
+++ Forge/src/app/service/auth-service.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class AuthServiceService {
 
   constructor() { }
+  logout(): void{
+    localStorage.setItem('isLoggedIn', 'false');
+    
+  }
 }
