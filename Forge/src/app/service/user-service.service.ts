311e0d745e456c77867a5d2a792f59373d2290fd
gabriela <gabskatzedev@gmail.com>
2020-10-27 19:25:42 -0400

    styling and portfolios table

--- Forge/src/app/service/user-service.service.ts
+++ Forge/src/app/service/user-service.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { UserHome } from 'src/app/user-home/userModel';
+import { Portfolio } from 'src/app/portfolio/portfolioModel';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  private getPortfoliosUrl: string = "http://localhost:8200/service/getAllPortfolios";
+
+  constructor(private http: HttpClient) { 
+
+  }
+  getPortfolios(): Observable<Portfolio[]>{
+    return this.http.get<Portfolio[]>(this.getPortfoliosUrl);
+  }
+
 }
