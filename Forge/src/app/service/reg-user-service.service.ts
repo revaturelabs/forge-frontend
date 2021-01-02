e57b29ec2624aac4ae20e11de8114505f0d84380
aquamiguel <36058940+aquamiguel@users.noreply.github.com>
2021-01-02 14:51:26 -0800

    Update reg-user-service.service.ts

--- Forge/src/app/service/reg-user-service.service.ts
+++ Forge/src/app/service/reg-user-service.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {User} from '../user';
+//user giving error in Jenkins
 
 @Injectable({
   providedIn: 'root'
