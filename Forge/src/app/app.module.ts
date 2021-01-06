daa831d551bc298132af130f3e9b6f72cddf0ee6
mounkeygirl <mounkeygirl@yahoo.com>
2021-01-06 17:14:45 -0600

    removed imports that were throwing errors on serve

--- Forge/src/app/app.module.ts
+++ Forge/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
@@ -19,7 +20,9 @@ import { WorkExperienceComponent } from './work-experience/work-experience.compo
 import { ProjectsComponent } from './projects/projects.component';
 import { AdminPortViewComponent } from './admin-port-view/admin-port-view.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardHeader, MatCardModule} from '@angular/material/card';
+// import { MatCardHeader, MatCardModule} from '@angular/material/card';
+import {  MatCardModule} from '@angular/material/card';
+
 import { RichTextEditorAllModule, ToolbarService} from '@syncfusion/ej2-angular-richtexteditor';
 import { MatIconModule } from '@angular/material/icon';
 import { ChartsModule } from 'ng2-charts';
@@ -69,13 +72,13 @@ import { NavbarAdminComponent } from './navbar-admin/navbar-admin.component';
     BrowserAnimationsModule,
     MatCardModule, 
     MatIconModule,
-    MatCardHeader,
+    // MatCardHeader,
     RichTextEditorAllModule,
     ChartsModule,
     NavbarModule,
     WavesModule,
     ButtonsModule,
-    FormBuilder,
+    // FormBuilder,
     MDBBootstrapModule.forRoot()
   ],
   providers: [ToolbarService],
